f19c7a6900df2b390b43af400b00f84ec1549234
엄유상 <dbtkd1801@gmail.com>
2025-08-05 13:42:08 +0900

    init: create project

---
+++ src/main/java/com/ssafy/ssashinsa/heyfy/HeyfyApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.ssashinsa.heyfy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeyfyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeyfyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssafy/ssashinsa/heyfy/HeyfyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.ssashinsa.heyfy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeyfyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
