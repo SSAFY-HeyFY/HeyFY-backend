ec7034bc9df9e0736840186ec00d7badcce20e0d
seungsang <rovin000@hansung.ac.kr>
2025-08-14 21:10:09 +0900

    #17 : 에러 처리 로직 수정

--- src/main/java/com/ssafy/ssashinsa/heyfy/common/ErrorCode.java
+++ src/main/java/com/ssafy/ssashinsa/heyfy/common/ErrorCode.java
@@ -16,6 +16,7 @@ public enum ErrorCode {
     LOGIN_FAILED(HttpStatus.UNAUTHORIZED, "아이디 또는 비밀번호가 틀립니다."),
 
 
+    TOKEN_PAIR_MISMATCH(HttpStatus.BAD_REQUEST, "액세스 토큰과 리프레쉬 토큰이 매치되지 않습니다"),
     MISSING_ACCESS_TOKEN(HttpStatus.BAD_REQUEST, "액세스 토큰이 포함되지 않았습니다."),
     MISSING_REFRESH_TOKEN(HttpStatus.BAD_REQUEST, "리프레쉬 토큰이 포함되지 않았습니다"),
     NOT_EXPIRED_TOKEN(HttpStatus.BAD_REQUEST, "만료되지 않은 액세스 토큰입니다");
