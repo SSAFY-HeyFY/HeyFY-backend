7a4a464ab5a6527f0270f9b28bd6c4be2cbf530c
eomyoosang <dbtkd1801@gmail.com>
2025-08-17 02:18:30 +0900

    #35: LogbookConfig 설정
    CustomBodyFilter를 통해 들어온 PII는 전부 블러처리됨

---
+++ src/main/java/com/ssafy/ssashinsa/heyfy/log/config/LogbookConfig.java
@@ -0,0 +1,31 @@
+package com.ssafy.ssashinsa.heyfy.log.config;
+
+import java.util.Set;
+import java.util.HashSet;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.zalando.logbook.Logbook;
+import org.zalando.logbook.core.Conditions;
+import org.zalando.logbook.core.HeaderFilters;
+import org.zalando.logbook.core.QueryFilters;
+
+@Configuration
+public class LogbookConfig {
+
+    @Bean
+    public Logbook logbook() {
+        Set<String> sensitiveKeys = new HashSet<>();
+        sensitiveKeys.add("password");
+        sensitiveKeys.add("accessToken");
+        sensitiveKeys.add("refreshToken");
+        sensitiveKeys.add("authorization");
+        sensitiveKeys.add("rrn");
+        sensitiveKeys.add("phone");
+        sensitiveKeys.add("cardNumber");
+        sensitiveKeys.add("accountNumber");
+
+        return Logbook.builder()
+                .bodyFilter(new CustomBodyFilter(sensitiveKeys))
+                .build();
+    }
+}
