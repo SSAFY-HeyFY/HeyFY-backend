eec42c68212874775d760d4e8915add44d5f1e30
eomyoosang <dbtkd1801@gmail.com>
2025-08-16 16:28:49 +0900

    #33: MaskingConverter 생성 및 테스트 작성

---
+++ src/main/java/com/ssafy/ssashinsa/heyfy/log/MaskingConverter.java
@@ -0,0 +1,12 @@
+package com.ssafy.ssashinsa.heyfy.log;
+
+import ch.qos.logback.classic.pattern.ClassicConverter;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+
+public class MaskingConverter extends ClassicConverter {
+
+    @Override
+    public String convert(ILoggingEvent event) {
+        return "";
+    }
+}
