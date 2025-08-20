b244890377af9b0b48cbbad28b6a1c786bf66ba0
jenny1zzang <cyberkhl@naver.com>
2025-08-21 02:35:18 +0900

    #83 : transfer controller & service 수정

--- src/main/java/com/ssafy/ssashinsa/heyfy/transfer/controller/TransferController.java
+++ src/main/java/com/ssafy/ssashinsa/heyfy/transfer/controller/TransferController.java
@@ -1,17 +1,14 @@
 package com.ssafy.ssashinsa.heyfy.transfer.controller;
 
+import com.ssafy.ssashinsa.heyfy.account.exception.AccountErrorCode;
 import com.ssafy.ssashinsa.heyfy.common.exception.CustomException;
 import com.ssafy.ssashinsa.heyfy.swagger.docs.ErrorsCommonDocs;
 import com.ssafy.ssashinsa.heyfy.transfer.docs.TransferDocs;
 import com.ssafy.ssashinsa.heyfy.transfer.dto.*;
-import com.ssafy.ssashinsa.heyfy.transfer.exception.TransferApiErrorCode;
 import com.ssafy.ssashinsa.heyfy.transfer.service.TransferService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.OffsetDateTime;
 import java.time.ZoneId;
@@ -25,26 +22,46 @@ public class TransferController {
 
     private final TransferService transferService;
 
-    @PostMapping
+    @PostMapping("/domestic")
     @TransferDocs
     public TransferHistoryResponse transfer(@RequestBody CreateTransferRequest req) {
-        EntireTransferResponseDto response = transferService.callTransfer(
-                req.withdrawalAccountNo(), req.depositAccountNo(), req.amount()
+        EntireTransferResponseDto transferResponse = transferService.callTransfer(
+                req.getDepositAccountNo(), req.getAmount()
         );
 
-        String responseCode = response.getHeader().getResponseCode();
-
-        if (!"H0000".equals(responseCode)) {
-            TransferApiErrorCode errorCode = TransferApiErrorCode.fromCode(responseCode);
-            System.out.println("responseCode = " + responseCode);
-            throw new CustomException(errorCode);
+        String withdrawalAccountNo = transferResponse.getREC().get(0).getAccountNo();
+        if (withdrawalAccountNo == null || withdrawalAccountNo.isEmpty()) {
+            throw new CustomException(AccountErrorCode.WITHDRAWAL_ACCOUNT_NOT_FOUND); // 계좌 없을 시 예외 처리
         }
 
-        var history = new TransferHistory(
-                req.withdrawalAccountNo(),
-                req.depositAccountNo(),
-                req.amount(),
+        TransferHistory history = new TransferHistory(
+                withdrawalAccountNo,
+                req.getDepositAccountNo(),
+                req.getAmount(),
                 "KRW",
+                OffsetDateTime.now(ZoneId.of("Asia/Seoul"))
+        );
+
+        return TransferHistoryResponse.ok(history);
+    }
+
+    @PostMapping("/foreign")
+    @TransferDocs
+    public TransferHistoryResponse foreignTransfer(@RequestBody CreateTransferRequest req) {
+        EntireTransferResponseDto transferResponse = transferService.callForeignTransfer(
+                req.getDepositAccountNo(), req.getAmount()
+        );
+
+        String withdrawalAccountNo = transferResponse.getREC().get(0).getAccountNo();
+        if (withdrawalAccountNo == null || withdrawalAccountNo.isEmpty()) {
+            throw new CustomException(AccountErrorCode.WITHDRAWAL_ACCOUNT_NOT_FOUND); // 계좌 없을 시 예외 처리
+        }
+
+        TransferHistory history = new TransferHistory(
+                withdrawalAccountNo,
+                req.getDepositAccountNo(),
+                req.getAmount(),
+                "USD",
                 OffsetDateTime.now(ZoneId.of("Asia/Seoul"))
         );
 
