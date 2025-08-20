11da94e479f805e07c9ea7097fc863dc65474d85
seungsang <rovin000@hansung.ac.kr>
2025-08-21 02:28:21 +0900

    #104 : 신한은행 계좌 조회, 거래 내역, 1원 인증 요청 로직 구현

--- src/main/java/com/ssafy/ssashinsa/heyfy/register/exception/ShinhanRegisterApiErrorCode.java
+++ src/main/java/com/ssafy/ssashinsa/heyfy/register/exception/ShinhanRegisterApiErrorCode.java
@@ -12,7 +12,8 @@ public enum ShinhanRegisterApiErrorCode implements ErrorCode {
     MISSING_USER_KEY(HttpStatus.BAD_REQUEST, "유저키가 누락되었습니다."),
     USER_NOT_FOUND(HttpStatus.INTERNAL_SERVER_ERROR, "유저를 찾을 수 없습니다."),
     RESPONSE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "신한API 응답 에러"),
-    ACCOUNT_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "이미 계좌가 존재하는 유저입니다."),;
+    ACCOUNT_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "이미 계좌가 존재하는 유저입니다."),
+    ACCOUNT_NOT_FOUND(HttpStatus.INTERNAL_SERVER_ERROR, "계좌를 찾을 수 없습니다."),;
 
     private final HttpStatus httpStatus;
     private final String message;
