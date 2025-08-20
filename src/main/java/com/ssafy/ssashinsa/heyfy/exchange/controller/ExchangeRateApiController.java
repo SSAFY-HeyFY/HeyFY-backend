6598627c54c9236aa8a7dc4cd2630e6a302db893
eomyoosang <dbtkd1801@gmail.com>
2025-08-20 22:33:26 +0900

    #81: ExchangeRateApiController api 추가 기간 별 환율 조회 api, 현재 환율 조회 api, 예측 제공 api, 환전일자 제공 api

--- src/main/java/com/ssafy/ssashinsa/heyfy/exchange/controller/ExchangeRateApiController.java
+++ src/main/java/com/ssafy/ssashinsa/heyfy/exchange/controller/ExchangeRateApiController.java
@@ -1,16 +1,16 @@
 package com.ssafy.ssashinsa.heyfy.exchange.controller;
 
+import com.ssafy.ssashinsa.heyfy.exchange.dto.*;
+import com.ssafy.ssashinsa.heyfy.exchange.service.ExchangeRateService;
 import com.ssafy.ssashinsa.heyfy.swagger.docs.ErrorsCommonDocs;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.ResponseEntity;
-
-import com.ssafy.ssashinsa.heyfy.exchange.dto.ExchangeRatePageResponseDto;
-import com.ssafy.ssashinsa.heyfy.exchange.service.ExchangeRateService;
 
 @Slf4j
 @ErrorsCommonDocs
@@ -25,5 +25,26 @@ public class ExchangeRateApiController {
     public ResponseEntity<ExchangeRatePageResponseDto> getExchangeRatePage() {
         ExchangeRatePageResponseDto response = exchangeRateService.getExchangeRatePage();
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/histories")
+    public ResponseEntity<ExchangeRateHistoriesDto> getExchangeRateHistories(@RequestParam(defaultValue="USD")String currency, @RequestParam(defaultValue = "29") Integer day) {
+        // 기본값: USD, 29일
+        return ResponseEntity.ok(exchangeRateService.getExchangeRateHistories(currency, day));
+    }
+
+    @GetMapping("/current")
+    public ResponseEntity<ExchangeRateGroupDto> getCurrentExchangeRates() {
+        return ResponseEntity.ok(exchangeRateService.getCurrentExchangeRates());
+    }
+
+    @GetMapping("/prediction")
+    public ResponseEntity<PredictionDto> getPrediction() {
+        return ResponseEntity.ok(exchangeRateService.getPrediction());
+    }
+
+    @GetMapping("/tuition")
+    public ResponseEntity<TuitionDto> getTuition() {
+        return ResponseEntity.ok(exchangeRateService.getTuition());
     }
 }
