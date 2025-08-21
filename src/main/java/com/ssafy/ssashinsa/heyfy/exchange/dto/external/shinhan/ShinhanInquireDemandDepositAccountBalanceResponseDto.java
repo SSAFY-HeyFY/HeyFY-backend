4a4dcc7a5112608d2e0a21ce06e1c3ba3603f4b8
eomyoosang <dbtkd1801@gmail.com>
2025-08-21 17:46:02 +0900

    #101: 신한 api 연동 dto 추가

---
+++ src/main/java/com/ssafy/ssashinsa/heyfy/exchange/dto/external/shinhan/ShinhanInquireDemandDepositAccountBalanceResponseDto.java
@@ -0,0 +1,19 @@
+package com.ssafy.ssashinsa.heyfy.exchange.dto.external.shinhan;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.ssafy.ssashinsa.heyfy.shinhanApi.dto.ShinhanCommonResponseHeaderDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ShinhanInquireDemandDepositAccountBalanceResponseDto {
+    @JsonProperty("Header")
+    ShinhanCommonResponseHeaderDto Header;
+    @JsonProperty("REC")
+    private ShinhanInquireDemandDepositAccountBalanceResponseRecDto REC;
+}
