8a023f18325c52e42fca40a7b33ba1b6e0736380
eomyoosang <dbtkd1801@gmail.com>
2025-08-21 17:43:48 +0900

    #101: (외환)계좌 이메일로 조회 로직 추가

--- src/main/java/com/ssafy/ssashinsa/heyfy/account/repository/ForeignAccountRepository.java
+++ src/main/java/com/ssafy/ssashinsa/heyfy/account/repository/ForeignAccountRepository.java
@@ -3,6 +3,7 @@ package com.ssafy.ssashinsa.heyfy.account.repository;
 import com.ssafy.ssashinsa.heyfy.account.domain.ForeignAccount;
 import com.ssafy.ssashinsa.heyfy.user.domain.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
@@ -11,5 +12,12 @@ public interface ForeignAccountRepository extends JpaRepository<ForeignAccount, 
 
     Optional<ForeignAccount> findByUser(Users user);
 
+    @Query("""
+            SELECT fa
+            FROM Users u
+            JOIN u.foreignAccount fa
+            WHERE LOWER(u.email) = LOWER(:email)
+            """)
+    Optional<ForeignAccount> findForeignAccountByUserEmail(String email);
 }
 
