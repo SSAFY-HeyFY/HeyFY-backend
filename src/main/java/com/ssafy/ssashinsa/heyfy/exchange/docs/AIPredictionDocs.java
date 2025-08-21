4048437042675f6066f8a4f64ff04f70065affb8
eomyoosang <dbtkd1801@gmail.com>
2025-08-21 23:34:17 +0900

    #117: 환전 페이지 api 문서작성

---
+++ src/main/java/com/ssafy/ssashinsa/heyfy/exchange/docs/AIPredictionDocs.java
@@ -0,0 +1,22 @@
+package com.ssafy.ssashinsa.heyfy.exchange.docs;
+
+import com.ssafy.ssashinsa.heyfy.common.exception.ErrorResponse;
+import com.ssafy.ssashinsa.heyfy.exchange.dto.exchange.AIPredictionResponseDto;
+import com.ssafy.ssashinsa.heyfy.exchange.dto.exchange.AccountBalanceResponseDto;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@ApiResponses({
+        @ApiResponse(responseCode = "200", description = "조회 성공",
+                content = @Content(schema = @Schema(implementation = AIPredictionResponseDto.class)))
+})
+public @interface AIPredictionDocs {
+}
