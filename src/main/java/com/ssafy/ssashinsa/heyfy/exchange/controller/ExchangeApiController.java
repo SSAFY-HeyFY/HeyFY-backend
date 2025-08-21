893f580f36e0304b14f081add09b8b1007f02e39
eomyoosang <dbtkd1801@gmail.com>
2025-08-21 21:26:38 +0900

    #101: 환전 화면 api

--- src/main/java/com/ssafy/ssashinsa/heyfy/exchange/controller/ExchangeApiController.java
+++ src/main/java/com/ssafy/ssashinsa/heyfy/exchange/controller/ExchangeApiController.java
@@ -1,7 +1,10 @@
 package com.ssafy.ssashinsa.heyfy.exchange.controller;
 
 import com.ssafy.ssashinsa.heyfy.authentication.annotation.AuthUser;
+import com.ssafy.ssashinsa.heyfy.exchange.dto.exchange.AIPredictionResponseDto;
 import com.ssafy.ssashinsa.heyfy.exchange.dto.exchange.AccountBalanceResponseDto;
+import com.ssafy.ssashinsa.heyfy.exchange.dto.exchange.ExchangePageResponseDto;
+import com.ssafy.ssashinsa.heyfy.exchange.dto.exchange.HistoricalAnalysisResponseDto;
 import com.ssafy.ssashinsa.heyfy.exchange.service.ExchangeService;
 import com.ssafy.ssashinsa.heyfy.swagger.docs.ErrorsCommonDocs;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -10,6 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,4 +32,34 @@ public class ExchangeApiController {
                 exchangeService.getAccountBalance(userDetails.getUsername())
         );
     }
+
+    @GetMapping("/foreign/account-balance")
+    public ResponseEntity<AccountBalanceResponseDto> getForeignAccountBalance(@AuthUser UserDetails userDetails) {
+        return ResponseEntity.ok(
+                exchangeService.getForeignAccountBalance(userDetails.getUsername())
+        );
+    }
+
+    @GetMapping("/ai-prediction")
+    public ResponseEntity<AIPredictionResponseDto> getAIPrediction(){
+        return ResponseEntity.ok(
+                exchangeService.getExchangeRateAIPrediction()
+        );
+    }
+
+    @GetMapping("/historical-analysis")
+    public ResponseEntity<HistoricalAnalysisResponseDto> getHistoricalAnalysis(){
+        return ResponseEntity.ok(
+                exchangeService.getHistoricalAnalysis()
+        );
+    }
+
+    @GetMapping("/page")
+    public ResponseEntity<ExchangePageResponseDto> getExchangePage(@AuthUser UserDetails userDetails){
+        return ResponseEntity.ok(
+                exchangeService.getExchangePage(userDetails.getUsername())
+        );
+    }
+
+
 }
