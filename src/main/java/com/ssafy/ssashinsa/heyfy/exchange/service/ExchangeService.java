5af3546a46fe32be47a643fe574e3ab7a7be0a7f
eomyoosang <dbtkd1801@gmail.com>
2025-08-21 21:26:07 +0900

    #101: 신한 api 호출부 분리

--- src/main/java/com/ssafy/ssashinsa/heyfy/exchange/service/ExchangeService.java
+++ src/main/java/com/ssafy/ssashinsa/heyfy/exchange/service/ExchangeService.java
@@ -10,18 +10,13 @@ import com.ssafy.ssashinsa.heyfy.exchange.dto.exchange.AIPredictionResponseDto;
 import com.ssafy.ssashinsa.heyfy.exchange.dto.exchange.AccountBalanceResponseDto;
 import com.ssafy.ssashinsa.heyfy.exchange.dto.exchange.ExchangePageResponseDto;
 import com.ssafy.ssashinsa.heyfy.exchange.dto.exchange.HistoricalAnalysisResponseDto;
-import com.ssafy.ssashinsa.heyfy.exchange.dto.external.shinhan.AccountBalanceRequestDto;
 import com.ssafy.ssashinsa.heyfy.exchange.dto.external.shinhan.ShinhanInquireDemandDepositAccountBalanceResponseDto;
 import com.ssafy.ssashinsa.heyfy.exchange.exception.ExchangeErrorCode;
-import com.ssafy.ssashinsa.heyfy.inquire.dto.ShinhanInquireDemandDepositResponseDto;
-import com.ssafy.ssashinsa.heyfy.shinhanApi.config.ShinhanApiClient;
-import com.ssafy.ssashinsa.heyfy.shinhanApi.exception.ShinhanApiErrorCode;
-import com.ssafy.ssashinsa.heyfy.shinhanApi.utils.ShinhanApiUtil;
+import com.ssafy.ssashinsa.heyfy.exchange.util.ShinhanExchangeApiClient;
 import com.ssafy.ssashinsa.heyfy.user.domain.Users;
 import com.ssafy.ssashinsa.heyfy.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -32,8 +27,7 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class ExchangeService {
 
-    private final ShinhanApiClient apiClient;
-    private final ShinhanApiUtil shinhanApiUtil;
+    private final ShinhanExchangeApiClient apiClient;
     private final UserRepository userRepository;
     private final AccountRepository accountRepository;
     private final ForeignAccountRepository foreignAccountRepository;
@@ -46,17 +40,33 @@ public class ExchangeService {
                 .orElseThrow(() -> new CustomException(ExchangeErrorCode.ACCOUNT_NOT_FOUND));
 
 
-        ShinhanInquireDemandDepositAccountBalanceResponseDto response = getAccountBalanceFromExternalApi(account.getAccountNo(), user.getUserKey());
+        ShinhanInquireDemandDepositAccountBalanceResponseDto response = apiClient.getAccountBalanceFromExternalApi(account.getAccountNo(), user.getUserKey());
 
         return AccountBalanceResponseDto.builder()
-                .accountNo(account.getAccountNo())
+                .accountNo(response.getREC().getAccountNo())
                 .accountBalance(Integer.parseInt(response.getREC().getAccountBalance()))
-                .currency("KRW")
+                .currency(response.getREC().getCurrency())
+                .build();
+    }
+    @Transactional
+    public AccountBalanceResponseDto getForeignAccountBalance(String studentId) {
+        Users user = userRepository.findByStudentId(studentId)
+                .orElseThrow(() -> new CustomException(AuthErrorCode.USER_NOT_FOUND));
+        ForeignAccount foreignAccount = foreignAccountRepository.findForeignAccountByUserEmail(user.getEmail())
+                .orElseThrow(() -> new CustomException(ExchangeErrorCode.ACCOUNT_NOT_FOUND));
+
+
+        ShinhanInquireDemandDepositAccountBalanceResponseDto response = apiClient.getForeignAccountBalanceFromExternalApi(foreignAccount.getAccountNo(), user.getUserKey());
+
+        return AccountBalanceResponseDto.builder()
+                .accountNo(foreignAccount.getAccountNo())
+                .accountBalance(Integer.parseInt(response.getREC().getAccountBalance()))
+                .currency(response.getREC().getCurrency())
                 .build();
     }
 
     @Transactional
-    public HistoricalAnalysisResponseDto getHIstoriaclAnalysis(){
+    public HistoricalAnalysisResponseDto getHistoricalAnalysis(){
         return HistoricalAnalysisResponseDto.builder()
                 .message("Over the past 30 days, today shows the highest exchange rate")
                 .build();
@@ -80,7 +90,7 @@ public class ExchangeService {
 
         // account balance
         ShinhanInquireDemandDepositAccountBalanceResponseDto accountBalance
-                = getAccountBalanceFromExternalApi(account.getAccountNo(), user.getUserKey());
+                = apiClient.getAccountBalanceFromExternalApi(account.getAccountNo(), user.getUserKey());
         AccountBalanceResponseDto accountBalanceResponseDto = AccountBalanceResponseDto.builder()
                 .accountNo(accountBalance.getREC().getAccountNo())
                 .accountBalance(Integer.parseInt(accountBalance.getREC().getAccountBalance()))
@@ -90,7 +100,7 @@ public class ExchangeService {
 
         // foreign account balance
         ShinhanInquireDemandDepositAccountBalanceResponseDto foreignAccountBalance
-                = getAccountBalanceFromExternalApi(foreignAccount.getAccountNo(), user.getUserKey());
+                = apiClient.getForeignAccountBalanceFromExternalApi(foreignAccount.getAccountNo(), user.getUserKey());
         AccountBalanceResponseDto foreignAccountBalanceResponseDto = AccountBalanceResponseDto.builder()
                 .accountNo(foreignAccountBalance.getREC().getAccountNo())
                 .accountBalance(Integer.parseInt(foreignAccountBalance.getREC().getAccountBalance()))
@@ -114,66 +124,8 @@ public class ExchangeService {
                 .build();
     }
 
-    private ShinhanInquireDemandDepositAccountBalanceResponseDto getAccountBalanceFromExternalApi(String accountNo, String userKey) {
-        AccountBalanceRequestDto requestDto = createAccountBalanceRequestDto(accountNo, userKey);
-        logRequest(requestDto);
-        ShinhanInquireDemandDepositAccountBalanceResponseDto response = apiClient.getClient("edu")
-                .post()
-                .uri("/demandDeposit/inquireDemandDepositAccountBalance")
-                .header("Content-Type", "application/json")
-                .bodyValue(requestDto)
-                .retrieve()
-                .onStatus(HttpStatusCode::isError, r ->
-                        r.bodyToMono(String.class).flatMap(body -> {
-                            log.error("API Error Body: {}", body);
-                            throw new CustomException(ShinhanApiErrorCode.API_CALL_FAILED);
-                        }))
-                .bodyToMono(ShinhanInquireDemandDepositAccountBalanceResponseDto.class)
-                .doOnNext(this::logResponse)
-                .block();
-        return response;
-    }
 
-    private ShinhanInquireDemandDepositResponseDto getAccountInfoFromExternalApi(String accountNo, String userKey) {
-        AccountBalanceRequestDto requestDto = createAccountBalanceRequestDto(accountNo, userKey);
-        logRequest(requestDto);
-        ShinhanInquireDemandDepositResponseDto response = apiClient.getClient("edu")
-                .post()
-                .uri("/demandDeposit/inquireDemandDepositAccount")
-                .header("Content-Type", "application/json")
-                .bodyValue(requestDto)
-                .retrieve()
-                .onStatus(HttpStatusCode::isError, r ->
-                        r.bodyToMono(String.class).flatMap(body -> {
-                            log.error("API Error Body: {}", body);
-                            throw new CustomException(ShinhanApiErrorCode.API_CALL_FAILED);
-                        }))
-                .bodyToMono(ShinhanInquireDemandDepositResponseDto.class)
-                .doOnNext(this::logResponse)
-                .block();
-        return response;
-    }
 
-    private AccountBalanceRequestDto createAccountBalanceRequestDto(String accountNo, String userKey) {
-        String apiKey = apiClient.getManagerKey();
-        return AccountBalanceRequestDto.builder()
-                .Header(shinhanApiUtil.createHeaderDto("inquireDemandDepositAccountBalance", "inquireDemandDepositAccountBalance", apiKey, userKey))
-                .accountNo(accountNo)
-                .build();
-    }
 
-    private void logRequest(Object requestDto) {
-        try {
-            log.info("Request JSON: {}", new com.fasterxml.jackson.databind.ObjectMapper().writeValueAsString(requestDto));
-        } catch (Exception e) {
-            log.error("Request logging error", e);
-        }
-    }
-    private void logResponse(Object responseDto) {
-        try {
-            log.info("Response JSON: {}", new com.fasterxml.jackson.databind.ObjectMapper().writeValueAsString(responseDto));
-        } catch (Exception e) {
-            log.error("Response logging error", e);
-        }
-    }
+
 }
