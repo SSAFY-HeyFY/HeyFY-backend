00cbc9b75ebbaa1c45406b9984cd150d62413e72
seungsang <rovin000@hansung.ac.kr>
2025-08-26 21:26:50 +0900

    #132 : HttpMessageNotReadableException 전역 에러 처리

--- src/main/java/com/ssafy/ssashinsa/heyfy/common/exception/CommonErrorCode.java
+++ src/main/java/com/ssafy/ssashinsa/heyfy/common/exception/CommonErrorCode.java
@@ -11,7 +11,9 @@ public enum CommonErrorCode implements ErrorCode {
     INVALID_FIELD(HttpStatus.BAD_REQUEST, "잘못된 필드입니다."),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "에러가 발생했습니다"),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "유저를 찾을 수 없습니다."),
-    Shinhan_API_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "은행 시스템 오류가 발생했습니다. 잠시 후 다시 시도해주세요.");
+    Shinhan_API_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "은행 시스템 오류가 발생했습니다. 잠시 후 다시 시도해주세요."),
+    INVALID_REQUEST_BODY(HttpStatus.BAD_REQUEST, "요청 본문이 없거나 형식이 유효하지 않습니다.");
+
 
     private final HttpStatus httpStatus;
     private final String message;
