a2859cb341a61438d57af45c9067f02d636a2004
jenny1zzang <cyberkhl@naver.com>
2025-08-27 01:09:53 +0900

    #142 : Swagger Docs 수정

--- src/main/java/com/ssafy/ssashinsa/heyfy/transfer/docs/ForeignTransferDocs.java
+++ src/main/java/com/ssafy/ssashinsa/heyfy/transfer/docs/ForeignTransferDocs.java
@@ -1,7 +1,7 @@
 package com.ssafy.ssashinsa.heyfy.transfer.docs;
 
 import com.ssafy.ssashinsa.heyfy.common.exception.ErrorResponse;
-import com.ssafy.ssashinsa.heyfy.transfer.dto.TransferHistoryResponse;
+import com.ssafy.ssashinsa.heyfy.transfer.dto.TransferHistory;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
@@ -15,46 +15,92 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
-@Operation(summary = "외환 계좌 이체", description = "계좌로 금액을 이체합니다.")
+@Operation(
+        summary = "외화 계좌 이체",
+        description = "사용자의 외화 출금 계좌에서 지정된 입금 계좌로 금액을 이체합니다.")
 @ApiResponses({
         @ApiResponse(
                 responseCode = "200",
                 description = "성공적으로 이체를 완료했습니다.",
                 content = @Content(
                         mediaType = "application/json",
-                        schema = @Schema(implementation = TransferHistoryResponse.class),
+                        schema = @Schema(implementation = TransferHistory.class),
                         examples = @ExampleObject(
-                                name = "계좌 이체 성공 예시",
-                                value = "{\n  \"success\": true,\n  \"history\": {\n    \"fromAccountMasked\": \"string\",\n    \"toAccountMasked\": \"string\",\n    \"amount\": \"string\",\n    \"currency\": \"USD\",\n    \"transactionSummary\": \"string\",\n    \"completedAt\": \"2025-08-23T23:13:49.593964+09:00\"\n  },\n  \"error\": null\n}"
+                                name = "원화 이체 성공",
+                                value = "{\n  \"depositAccountNo\": \"110123456789\",\n  \"amount\": \"10000\",\n  \"currency\": \"KRW\",\n  \"transactionSummary\": \"점심값\",\n  \"completedAt\": \"2025-08-26T23:50:15.123456+09:00\"\n}"
                         )
                 )
         ),
         @ApiResponse(
                 responseCode = "400",
-                description = "잘못된 요청: 출금 계좌를 찾을 수 없음",
+                description = "잘못된 요청",
                 content = @Content(
                         mediaType = "application/json",
                         schema = @Schema(implementation = ErrorResponse.class),
-                        examples = @ExampleObject(
-                                name = "출금 계좌 없음",
-                                summary = "현재 유저에게 연결된 출금 가능한 계좌가 없는 경우",
-                                value = "{\"status\": 400, \"httpError\": \"BAD_REQUEST\", \"errorCode\": \"WITHDRAWAL_ACCOUNT_NOT_FOUND\", \"message\": \"[A1003] 계좌번호가 유효하지 않습니다.\"}"
-                        )
+                        examples = {
+                                @ExampleObject(
+                                        name = "PIN 번호 불일치",
+                                        summary = "PIN 번호 오류",
+                                        value = "{\"status\": 400, \"httpError\": \"BAD_REQUEST\", \"errorCode\": \"INVALID_PIN_NUMBER\", \"message\": \"Wrong pin number\"}"
+                                ),
+                                @ExampleObject(
+                                        name = "금액 오류",
+                                        summary = "입력한 금액이 유효하지 않음",
+                                        value = "{\"status\": 400, \"httpError\": \"BAD_REQUEST\", \"errorCode\": \"INVALID_TRANSACTION_AMOUNT\", \"message\": \"The transaction amount is invalid\"}"
+                                ),
+                                @ExampleObject(
+                                        name = "잔액 부족",
+                                        summary = "잔액 부족",
+                                        value = "{\"status\": 400, \"httpError\": \"BAD_REQUEST\", \"errorCode\": \"INSUFFICIENT_BALANCE\", \"message\": \"Transaction failed due to insufficient account balance\"}"
+                                ),
+                                @ExampleObject(
+                                        name = "계좌번호 오류",
+                                        summary = "계좌번호 유효성 오류",
+                                        value = "{\"status\": 400, \"httpError\": \"BAD_REQUEST\", \"errorCode\": \"INVALID_ACCOUNT_NUMBER\", \"message\": \"The account number is invalid\"}"
+                                ),
+                                @ExampleObject(
+                                        name = "거래 내용 길이 초과",
+                                        summary = "거래 내용 길이 초과",
+                                        value = "{\"status\": 400, \"httpError\": \"BAD_REQUEST\", \"errorCode\": \"EXCEEDED_TRANSACTION_SUMMARY_LENGTH\", \"message\": \"The transaction summary length has been exceeded\"}"
+                                ),
+                                @ExampleObject(
+                                        name = "외화 계좌 아님",
+                                        summary = "외화 계좌가 아님",
+                                        value = "{\"status\": 400, \"httpError\": \"BAD_REQUEST\", \"errorCode\": \"ONLY_FOREIGN_CURRENCY_ACCOUNT\", \"message\": \"Only foreign currency accounts are allowed\"}"
+                                ),
+                                @ExampleObject(
+                                        name = "유저 키 누락",
+                                        summary = "인증 정보에서 유저 키를 찾을 수 없음",
+                                        value = "{\"status\": 400, \"httpError\": \"UNAUTHORIZED\", \"errorCode\": \"MISSING_USER_KEY\", \"message\": \"유저키가 누락되었습니다.\"}"
+                                ),
+                                @ExampleObject(
+                                        name = "유저 없음",
+                                        summary = "인증 정보에서 유저를 찾을 수 없음",
+                                        value = "{\"status\": 400, \"httpError\": \"UNAUTHORIZED\", \"errorCode\": \"USER_NOT_FOUND\", \"message\": \"유저를 찾을 수 없습니다.\"}"
+                                )
+                        }
                 )
         ),
-        @ApiResponse(
-                responseCode = "500",
-                description = "서버 내부 오류 또는 API 호출 실패",
-                content = @Content(
-                        mediaType = "application/json",
-                        schema = @Schema(implementation = ErrorResponse.class),
-                        examples = @ExampleObject(
-                                name = "API 호출 실패",
-                                summary = "신한은행 이체 API 통신 중 오류가 발생한 경우",
-                                value = "{\n  \"status\": 500,\n  \"httpError\": \"INTERNAL_SERVER_ERROR\",\n  \"errorCode\": \"API_CALL_FAILED\",\n  \"message\": \"신한 API 호출에 실패했습니다.\"\n}"
-                        )
-                )
-        )
+//        @ApiResponse(
+//                responseCode = "400",
+//                description = "인증 실패",
+//                content = @Content(
+//                        mediaType = "application/json",
+//                        schema = @Schema(implementation = ErrorResponse.class),
+//                        examples = {
+//                                @ExampleObject(
+//                                        name = "유저 키 누락",
+//                                        summary = "인증 정보에서 유저 키를 찾을 수 없음",
+//                                        value = "{\"status\": 400, \"httpError\": \"UNAUTHORIZED\", \"errorCode\": \"MISSING_USER_KEY\", \"message\": \"유저키가 누락되었습니다.\"}"
+//                                ),
+//                                @ExampleObject(
+//                                        name = "유저 없음",
+//                                        summary = "인증 정보에서 유저를 찾을 수 없음",
+//                                        value = "{\"status\": 400, \"httpError\": \"UNAUTHORIZED\", \"errorCode\": \"USER_NOT_FOUND\", \"message\": \"유저를 찾을 수 없습니다.\"}"
+//                                ),
+//                        }
+//                )
+//        )
 })
 public @interface ForeignTransferDocs {
 }
