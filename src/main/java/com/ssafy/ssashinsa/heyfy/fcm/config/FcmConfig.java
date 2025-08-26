90ca8f2fa021e9eb7ba6a5da55a27b10acbdf08b
jenny1zzang <cyberkhl@naver.com>
2025-08-26 10:30:23 +0900

    #150 : fcmconfig 추가

---
+++ src/main/java/com/ssafy/ssashinsa/heyfy/fcm/config/FcmConfig.java
@@ -0,0 +1,34 @@
+package com.ssafy.ssashinsa.heyfy.fcm.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@Configuration
+public class FcmConfig {
+
+    @PostConstruct
+    public void initialize() {
+        try {
+            String path = "heyfy-901bb-firebase-adminsdk-fbsvc-eb1c8680a8.json";
+            ClassPathResource resource = new ClassPathResource(path);
+            InputStream serviceAccount = resource.getInputStream();
+
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
