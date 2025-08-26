3c7184e552af57785623dbc4abf5cfcc134548aa
김승상 <rovin054@gmail.com>
2025-08-26 18:01:43 +0900

    #132 : 스웨거 변경, dto 추가

--- src/main/java/com/ssafy/ssashinsa/heyfy/authentication/docs/AuthSignInDocs.java
+++ src/main/java/com/ssafy/ssashinsa/heyfy/authentication/docs/AuthSignInDocs.java
@@ -21,7 +21,12 @@ import java.lang.annotation.Target;
                 content = @Content(schema = @Schema(implementation = SignInSuccessDto.class),
                         examples = @ExampleObject(
                                 name = "로그인 성공 응답",
-                                value = "{\"accessToken\":\"eyJ...\", \"refreshToken\":\"eyJ...\"}"
+                                // 💡 요청하신 JSON 데이터를 그대로 삽입
+                                value = "{\n" +
+                                        "  \"accessToken\": \"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI3Nzc3Nzc3Iiwicm9sZXMiOiIiLCJqdGkiOiJkODNmMTcxNC0zZjNlLTQzYjEtODhkOS0zNjhmYjcwMzhhNzQiLCJpYXQiOjE3NTYxOTQ0NjQsImV4cCI6MTc1NjE5ODA2NH0.WWnpJAxA8N2E8g3YC8QMJBQs-UADfEnNaJBiL3B_Xr0\",\n" +
+                                        "  \"refreshToken\": \"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI3Nzc3Nzc3IiwianRpIjoiZDgzZjE3MTQtM2YzZS00M2IxLTg4ZDktMzY4ZmI3MDM4YTc0IiwiaWF0IjoxNzU2MTk0NDY0LCJleHAiOjE3NTY3OTkyNjR9.Skw1n1-VjA1NZcYagrRF9gYvYjVVJ-8vvqs3yHbU2cE\",\n" +
+                                        "  \"sid\": \"959f77dc-c708-46d0-a9c1-abd5b4b9d011\"\n" +
+                                        "}"
                         ))),
         @ApiResponse(responseCode = "400", description = "로그인 실패",
                 content = @Content(schema = @Schema(implementation = ErrorResponse.class),
@@ -34,3 +39,9 @@ import java.lang.annotation.Target;
 })
 public @interface AuthSignInDocs {
 }
+
+
+
+
+
+
---
+++ src/main/java/com/ssafy/ssashinsa/heyfy/authentication/dto/SidDto.java
@@ -0,0 +1,14 @@
+package com.ssafy.ssashinsa.heyfy.authentication.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Schema(description = "sid 응답 DTO")
+public class SidDto {
+    private String sid;
+}
