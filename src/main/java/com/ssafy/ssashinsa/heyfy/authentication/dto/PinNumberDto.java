8f5be55199fceb272c3b4b2369b1184dc056d8aa
김승상 <rovin054@gmail.com>
2025-08-27 09:27:40 +0900

    #165 : pin 번호를 활용한 2차인증 로직 구현

---
+++ src/main/java/com/ssafy/ssashinsa/heyfy/authentication/dto/PinNumberDto.java
@@ -0,0 +1,13 @@
+package com.ssafy.ssashinsa.heyfy.authentication.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class PinNumberDto {
+    private String pinNumber;
+}
