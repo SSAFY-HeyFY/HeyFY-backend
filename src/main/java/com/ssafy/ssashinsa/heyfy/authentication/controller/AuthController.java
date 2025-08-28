4f3afeddc41fc70ea09dac3f4e0fdaab2b142565
김승상 <rovin054@gmail.com>
2025-08-28 14:38:03 +0900

    #125 : pin번호 재발급 API 변경

--- src/main/java/com/ssafy/ssashinsa/heyfy/authentication/controller/AuthController.java
+++ src/main/java/com/ssafy/ssashinsa/heyfy/authentication/controller/AuthController.java
@@ -1,12 +1,12 @@
 package com.ssafy.ssashinsa.heyfy.authentication.controller;
 
-import com.ssafy.ssashinsa.heyfy.authentication.docs.AuthCheckPinDocs;
 import com.ssafy.ssashinsa.heyfy.authentication.docs.AuthRefreshDocs;
 import com.ssafy.ssashinsa.heyfy.authentication.docs.AuthSignInDocs;
 import com.ssafy.ssashinsa.heyfy.authentication.docs.AuthSignUpDocs;
 import com.ssafy.ssashinsa.heyfy.authentication.docs.SidRefreshDocs;
 import com.ssafy.ssashinsa.heyfy.authentication.dto.*;
 import com.ssafy.ssashinsa.heyfy.authentication.dto.test.MessageDto;
+import com.ssafy.ssashinsa.heyfy.authentication.exception.AuthErrorCode;
 import com.ssafy.ssashinsa.heyfy.authentication.service.AuthService;
 import com.ssafy.ssashinsa.heyfy.common.exception.CustomException;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -48,7 +48,10 @@ public class AuthController {
         try {
             newSid = authService.issueSid(requestDto.getPinNumber());
         }catch(CustomException e){
-            return ResponseEntity.ok(new SidDto(newSid, false));
+            if(e.getErrorCode().equals(AuthErrorCode.INVALID_PIN_NUMBER)){
+                return ResponseEntity.ok(new SidDto(newSid, false));
+            }
+            throw e;
         }
         return ResponseEntity.ok(new SidDto(newSid, true));
     }
@@ -69,12 +72,12 @@ public class AuthController {
     }
 
 
-    @AuthCheckPinDocs
-    @PostMapping("/checkpin")
-    public ResponseEntity<CheckPinResponseDto> checkPin(@RequestBody PinNumberDto pinNumberDto) {
-        CheckPinResponseDto response = authService.checkPin(pinNumberDto.getPinNumber());
-
-        return ResponseEntity.ok(response);
-    }
+//    @AuthCheckPinDocs
+//    @PostMapping("/checkpin")
+//    public ResponseEntity<CheckPinResponseDto> checkPin(@RequestBody PinNumberDto pinNumberDto) {
+//        CheckPinResponseDto response = authService.checkPin(pinNumberDto.getPinNumber());
+//
+//        return ResponseEntity.ok(response);
+//    }
 
 }
