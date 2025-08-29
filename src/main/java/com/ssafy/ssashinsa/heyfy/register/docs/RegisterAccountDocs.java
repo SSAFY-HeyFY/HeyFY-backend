cd60724b1b6d45c65512d289d69ed675d9eea964
김승상 <rovin054@gmail.com>
2025-08-29 12:01:06 +0900

    #193 : 계좌 등록 로직에서 미사용 코드 제거, 스웨거 수정

--- src/main/java/com/ssafy/ssashinsa/heyfy/register/docs/RegisterAccountDocs.java
+++ src/main/java/com/ssafy/ssashinsa/heyfy/register/docs/RegisterAccountDocs.java
@@ -3,13 +3,12 @@ package com.ssafy.ssashinsa.heyfy.register.docs;
 import com.ssafy.ssashinsa.heyfy.account.dto.AccountNoDto;
 import com.ssafy.ssashinsa.heyfy.common.exception.ErrorResponse;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -71,7 +70,5 @@ import java.lang.annotation.Target;
                 )
         )
 })
-@Parameter(name = "Authorization", description = "JWT 액세스 토큰 (Bearer <token>)", in = ParameterIn.HEADER)
-@Parameter(name = "sid", description = "세션 ID", in = ParameterIn.HEADER)
 public @interface RegisterAccountDocs {
 }
