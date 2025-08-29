63fa6835febcc138da735e5ad7847d35d432896b
jenny1zzang <cyberkhl@naver.com>
2025-08-29 17:37:41 +0900

    #197 : 스케줄러 환경변수 설정

--- src/main/java/com/ssafy/ssashinsa/heyfy/fcm/scheduler/NotificationScheduler.java
+++ src/main/java/com/ssafy/ssashinsa/heyfy/fcm/scheduler/NotificationScheduler.java
@@ -20,7 +20,7 @@ public class NotificationScheduler {
     private final FcmService fcmService;
     private final RestTemplate restTemplate;
 
-    @Scheduled(cron = "0 * * * * *", zone = "Asia/Seoul")
+    @Scheduled(cron = "${notification.schedule.cron}", zone = "Asia/Seoul")
     public void sendDailyExchangeRateNotification() {
         log.info("스케줄러 실행: 환율 정보를 가져와 모든 사용자에게 알림을 보냅니다.");
 
