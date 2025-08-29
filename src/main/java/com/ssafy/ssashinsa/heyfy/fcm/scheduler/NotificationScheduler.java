051f7d48677f5822a18e27e9ada726369cd81ec2
jenny1zzang <cyberkhl@naver.com>
2025-08-29 17:34:45 +0900

    #197 : 스케줄러 수정

--- src/main/java/com/ssafy/ssashinsa/heyfy/fcm/scheduler/NotificationScheduler.java
+++ src/main/java/com/ssafy/ssashinsa/heyfy/fcm/scheduler/NotificationScheduler.java
@@ -1,33 +1,46 @@
 package com.ssafy.ssashinsa.heyfy.fcm.scheduler;
 
-import com.ssafy.ssashinsa.heyfy.fastapi.client.FastApiClient;
-import com.ssafy.ssashinsa.heyfy.fastapi.dto.FastApiRateStatusResponseDto;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.ssafy.ssashinsa.heyfy.fcm.domain.FcmToken;
+import com.ssafy.ssashinsa.heyfy.fcm.dto.ExchangeRateResponse;
 import com.ssafy.ssashinsa.heyfy.fcm.repository.FcmTokenRepository;
 import com.ssafy.ssashinsa.heyfy.fcm.service.FcmService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class NotificationScheduler {
-
-    private final FcmTokenRepository fcmTokenRepository;
     private final FcmService fcmService;
-    private final FastApiClient fastApiClient;
+    private final RestTemplate restTemplate;
 
-    @Scheduled(cron = "0 0 7 * * *", zone = "Asia/Seoul")
+    @Scheduled(cron = "0 * * * * *", zone = "Asia/Seoul")
     public void sendDailyExchangeRateNotification() {
         log.info("스케줄러 실행: 환율 정보를 가져와 모든 사용자에게 알림을 보냅니다.");
-        FastApiRateStatusResponseDto response = fastApiClient.getRateStatus();
-        if (response != null && response.getMessage() != null && !response.getMessage().isEmpty()) {
-            String title = "Today's Exchange Rate Information";
-            String body = response.getMessage();
 
-            fcmService.sendNotificationToAll(title, body);
+        try {
+            String apiUrl = "http://114.199.133.118:8888/api/push/rate-status";
+            ExchangeRateResponse response = restTemplate.getForObject(apiUrl, ExchangeRateResponse.class);
+
+            if (response != null && response.getMessage() != null && !response.getMessage().isEmpty()) {
+                String title = "Today's Exchange Rates";
+                String body = response.getMessage();
+
+
+                fcmService.sendNotificationToAll(title, body);
+            } else {
+                log.warn("API로부터 유효한 메시지를 받지 못했습니다.");
+            }
+
+        } catch (Exception e) {
+            // API 호출 실패 등 예외 발생 시 로그 기록
+            log.error("외부 API 호출 또는 알림 발송 중 오류가 발생했습니다.", e);
         }
-
     }
 }
